ade7c0693f8b1ba43becac10a1a912ca298963bc
Spencer Lindemuth <lindemut@gmail.com>
2019-09-04 13:14:51 -0700

    Create lessons and make unique id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :lessons, only: [:index]
+    resources :lessons, only: [:index, :create]
     resources :users, only: [:create]
     post '/login', to: 'auth#create'
     get '/profile', to: 'users#profile'
